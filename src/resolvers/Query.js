883e19fe8d61f987d0b819bca32cc6833a81bb37
Jack Doyle <jackdo@hotmail.co.uk>
2019-07-21 19:32:43 +0100

    Add order query resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -31,6 +31,28 @@ const Query = {
     // info variable contains the graphQL query from the frontend
     return ctx.db.query.users({}, info);
   },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!');
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd');
+    }
+    // 4. Return the order
+    return order;
+  },
+};
 };
 
 module.exports = Query;
